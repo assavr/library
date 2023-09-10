f21b0f07f828307b25c6ad7ab067b9f9811a49c9
Anastasia Savrukhina <assavr0@gmail.com>
2023-09-11 02:31:14 +0300

    feat: add register

--- library/js/index-2.js
+++ library/js/index-2.js
@@ -1,3 +1,4 @@
+
 // Бургер меню
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("button__burger-open").addEventListener("click", function () {
@@ -24,7 +25,7 @@ document.body.addEventListener('click', event => {
 
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("header__list").addEventListener("click", function () {
-      document.querySelector(".header").classList.toggle("open")
+    document.querySelector(".header").classList.toggle("open")
   })
 });
 
@@ -59,15 +60,15 @@ liButtons.forEach((dot, index) => {
 })
 
 arrowLeft.addEventListener('click', () => {
-  if (position < ((carouselButtons.length - 1) * 475) ) {
+  if (position < ((carouselButtons.length - 1) * 475)) {
     position += 475;
     dotIndex++;
   } else {
     position = (carouselButtons.length - 1) * 475;
     dotIndex = carouselButtons.length - 1;
   }
-    blockImg.style.left = -position + 'px'
-    dotsActive(dotIndex)
+  blockImg.style.left = -position + 'px'
+  dotsActive(dotIndex)
 })
 
 arrowRight.addEventListener('click', () => {
@@ -78,8 +79,8 @@ arrowRight.addEventListener('click', () => {
     position = 0
     dotIndex = 0
   }
-    blockImg.style.left = -position + 'px'
-    dotsActive(dotIndex)
+  blockImg.style.left = -position + 'px'
+  dotsActive(dotIndex)
 })
 
 // Слайдер сезонов блок Favorites
@@ -90,7 +91,7 @@ const labelClick = document.querySelectorAll('.favorites__label');
 labelClick[0].addEventListener('click', () => {
   for (let item of favoritesUl) {
     item.classList.add('favorites__list_none')
-  } 
+  }
   // setTimeout( () => favoritesUl[0].classList.remove('favorites__list_none'), 1000)
   favoritesUl[0].classList.remove('favorites__list_none')
 })
@@ -98,24 +99,24 @@ labelClick[0].addEventListener('click', () => {
 labelClick[1].addEventListener('click', () => {
   for (let item of favoritesUl) {
     item.classList.add('favorites__list_none')
-  } 
+  }
   // setTimeout( () => favoritesUl[1].classList.remove('favorites__list_none'), 1000)
   favoritesUl[1].classList.remove('favorites__list_none')
 })
 labelClick[2].addEventListener('click', () => {
   for (let item of favoritesUl) {
     item.classList.add('favorites__list_none')
-  } 
+  }
   // setTimeout( () => favoritesUl[2].classList.remove('favorites__list_none'), 1000)
   favoritesUl[2].classList.remove('favorites__list_none')
 })
 labelClick[3].addEventListener('click', () => {
   for (let item of favoritesUl) {
     item.classList.add('favorites__list_none')
-  } 
+  }
   // setTimeout( () => favoritesUl[3].classList.remove('favorites__list_none'), 1000)
   favoritesUl[3].classList.remove('favorites__list_none')
-  
+
 })
 
 
@@ -125,6 +126,7 @@ labelClick[3].addEventListener('click', () => {
 const white = document.querySelector('.white');
 const userButton = document.querySelector('.button__user-profile');
 const headerModal = document.querySelector('.header__profile');
+const headerModalAfter = document.querySelector('.header__profile__after');
 const userButton2 = document.querySelector('.button__user-profile-menu');
 const burgerOpenButton = document.querySelector('.button__burger-open');
 const burgerCloseButton = document.querySelector('.button__burger');
@@ -136,6 +138,8 @@ function closeAll() {
   registrationForm.classList.add('none')
   loginForm.classList.add('none')
   headerModal.classList.add('none');
+  headerModalAfter.classList.add('none')
+  windowMyProfile.classList.add('none')
 }
 
 
@@ -183,6 +187,7 @@ const registerCloseBtn = document.querySelector('.register-close__button');
 const loginCloseBtn = document.querySelector('.login-close__button');
 const librSignUp = document.querySelector('.button-sign')
 const librLogIn = document.querySelector('.button-log')
+const profileFromCard = document.querySelector('.button-sign-after')
 
 
 const gray = document.querySelector('.gray');
@@ -208,6 +213,7 @@ loginButton.addEventListener('click', () => {
 gray.addEventListener('click', closeAll)
 
 registerCloseBtn.addEventListener('click', closeAll)
+
 
 loginCloseBtn.addEventListener('click', closeAll)
 
@@ -242,36 +248,96 @@ librLogIn.addEventListener('click', () => {
 
 const inputsRegister = document.querySelectorAll('.window-register__input')
 const signUPBtn = document.querySelector('.window-register__button');
-
 const libraryAfter = document.querySelector('.block__check-the-card')
 const libraryBefore = document.querySelector('.library-card__container')
+const userButtonAfter = document.querySelector('.icon-user__after')
+const myProfileFromIcon = document.querySelector('.btn__my-profile');
+const LogOut = document.querySelector('.btn-log-out');
+const windowMyProfile = document.querySelector('.window__my-profile')
+const letterName = document.querySelectorAll('.icon-span')
+const userName = document.querySelector('.window__my-profile__name')
 
-const headerModalAfter = document.querySelector('.header__profile__after');
+function change() {
+  for (let i = 0; i < inputsRegister.length; i++) {
+    inputsRegister[i].addEventListener('change', changeHandler)
+  }
+}
 
 function changeHandler() {
   localStorage.setItem(this.name, this.value)
 }
 
-for (let i = 0; i < inputsRegister.length; i++) {
-  inputsRegister[i].addEventListener('change', changeHandler)
-}
+
 
 
 signUPBtn.addEventListener('click', () => {
   localStorage.setItem('register', 'true')
-  location.reload()
- 
+  location.reload();
 })
 
+myProfileFromIcon.addEventListener('click', () => {
+  // windowMyProfile.classList.remove('none');
+
+})
+
+LogOut.addEventListener('click', () => {
+  localStorage.setItem('register', 'false')
+  location.reload();
+})
+
+// Забираю буковки у имени фамилии 
+const first = localStorage.firstName;
+const last = localStorage.lastName;
+let letters = first.toUpperCase().slice(0,1) + last.toUpperCase().slice(0, 1)
+
+// Делаю случайное число
+// // Math.ceil(Math.random() * 1000000000)
+// let cardNumber;
+// function randomNum() {
+//   cardNumber = Math.ceil(Math.random() * 1000000000);
+// }
+// Не сделала :) 
+
+// Имя держателя карты 
+const readerNameAfter = document.querySelector('.input__check-the-card');
+const test = `${first + ' ' + last}`
+readerNameAfter.setAttribute('placeholder', test)
+
+profileFromCard.addEventListener('click', () => {
+windowMyProfile.classList.remove('none');
+gray.classList.remove('none');
+})
 
 function isRegister() {
-  if(localStorage.register === 'true') {
-    userButton.addEventListener('click', () => {
+  if (localStorage.register === 'true') {
+    userButtonAfter.classList.remove('none');
+    userButton.classList.add('none')
+    
+    userButtonAfter.addEventListener('click', () => {
       headerModalAfter.classList.toggle('none')
+      white.classList.toggle('none')
     })
+
+    myProfileFromIcon.addEventListener('click', () => {
+      windowMyProfile.classList.remove('none');
+      gray.classList.remove('none')
+      headerModalAfter.classList.add('none');
+      white.classList.add('none')
+    })
+    for (item of letterName) {
+    item.innerHTML = letters;
+    }
+
+    userName.innerHTML = `${first + ' ' + last}`;
+    
     libraryAfter.classList.remove('none');
     libraryBefore.classList.add('none');
   }
 }
 
 isRegister();
+change();
+
+
+
+// alert(localStorage.FirstName)
